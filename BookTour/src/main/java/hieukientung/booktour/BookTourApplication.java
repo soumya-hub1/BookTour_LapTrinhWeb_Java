b2fdabdbe7b75b0d86e8bbe40dd2bf5d5a0ebd06
minhhieupham2002 <minhhieupham2002@gmail.com>
2023-05-30 21:46:29 +0700

    Create prj

---
+++ BookTour/src/main/java/hieukientung/booktour/BookTourApplication.java
@@ -0,0 +1,13 @@
+package hieukientung.booktour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTourApplication.class, args);
+    }
+
+}
---
+++ BookTour/src/test/java/hieukientung/booktour/BookTourApplicationTests.java
@@ -0,0 +1,13 @@
+package hieukientung.booktour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
