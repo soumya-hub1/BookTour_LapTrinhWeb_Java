76cfac08b6cdc0a9250c920cb52b4ff8b5e979c6
minhhieupham2002 <minhhieupham2002@gmail.com>
2023-06-07 22:09:12 +0700

    update home controller, index

--- BookTour/src/main/java/hieukientung/booktour/controller/HomeController.java
+++ BookTour/src/main/java/hieukientung/booktour/controller/HomeController.java
@@ -1,23 +1,43 @@
 package hieukientung.booktour.controller;
 
+import hieukientung.booktour.model.Tour;
 import hieukientung.booktour.service.HomeService;
+import hieukientung.booktour.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class HomeController {
     @Autowired
     private HomeService homeService;
 
+    @Autowired
+    private TourService tourService;
+
     @GetMapping("/")
     public String index(Model model) {
+        List<Tour> allTours = tourService.getAllTours();
+
+        if (allTours.size() <= 6 || allTours.isEmpty()) {
+            return "index";
+        }
+        List<Tour> randomTours = randomTours(allTours, 6);
         model.addAttribute("listDestination", homeService.getAllDestinationPoint());
         model.addAttribute("listDeparture", homeService.getAllDeparturePoint());
+        model.addAttribute("listTours", randomTours);
         return "index";
+    }
+
+    private List<Tour> randomTours(List<Tour> allTours, int count) {
+        List<Tour> tourCopy = new ArrayList<>(allTours);
+        Collections.shuffle(tourCopy);
+        return tourCopy.subList(0, count);
     }
 }
 
