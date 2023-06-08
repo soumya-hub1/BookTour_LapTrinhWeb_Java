5f031fe3c8f7b21d585ce84a8d3a55e392bc6da4
minhhieupham2002 <minhhieupham2002@gmail.com>
2023-06-08 22:01:03 +0700

    remove attribute text uppercase

--- BookTour/src/main/java/hieukientung/booktour/controller/AdminController.java
+++ BookTour/src/main/java/hieukientung/booktour/controller/AdminController.java
@@ -5,9 +5,12 @@ import hieukientung.booktour.service.AdminService;
 import hieukientung.booktour.service.HomeService;
 import hieukientung.booktour.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/admin")
 @Controller
@@ -21,16 +24,16 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+
+    @GetMapping("")
+    public String getAllTours(Model model) {
+        return findPaginated(1, model);
+    }
+
     @PostMapping("/save")
     public String saveTour(@ModelAttribute("tour") Tour tour) {
         tourService.saveTour(tour);
         return "redirect:/admin";
-    }
-
-    @GetMapping("")
-    public String getAllTours(Model model) {
-        model.addAttribute("listTours", tourService.getAllTours());
-        return "admin/view-list-tours";
     }
 
     @GetMapping("/view-detail-tour/{id}")
@@ -70,5 +73,23 @@ public class AdminController {
     public String deleteTour(@PathVariable("id") Long id) {
         tourService.deleteTour(id);
         return "redirect:/admin/view-list-tours";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo, Model model) {
+        int pageSize = 6;
+        Page<Tour> page = tourService.findPaginated(pageNo, pageSize);
+        List<Tour> listTours = page.getContent();
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        int startPage = Math.max(1, pageNo - 2);
+        int endPage = Math.min(startPage + 4, page.getTotalPages());
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("listTours", listTours);
+        return "admin/view-list-tours";
     }
 }
