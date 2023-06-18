4702f4ab4e9f918fe537776b5dee37a57ba8e48d
Tùng Phan <sneakiedward@gmail.com>
2023-06-19 01:35:18 +0700

    css email-message

--- BookTour/src/main/resources/static/assets/js/ReceiveMail.js
+++ BookTour/src/main/resources/static/assets/js/ReceiveMail.js
@@ -10,12 +10,12 @@ $(document).ready(function () {
             data: {email: email}, // Dữ liệu gửi đi
             success: function (response) {
                 $('#errorMessage').empty(); // Xóa thông báo lỗi trước đó
-                $('#errorMessage').removeClass('alert alert-danger').addClass('alert alert-success');
+                $('#errorMessage').removeClass('alert alert-danger form-control').addClass('alert alert-success form-control');
                 $('#errorMessage').text(response); // Hiển thị thông báo thành công
             },
             error: function (xhr, status, error) {
                 var errorMessage = xhr.responseText; // Lấy thông báo lỗi từ phản hồi
-                $('#errorMessage').removeClass('alert alert-success').addClass('alert alert-danger');
+                $('#errorMessage').removeClass('alert alert-success form-control').addClass('alert alert-danger form-control');
                 $('#errorMessage').text(errorMessage); // Hiển thị thông báo lỗi
             }
         });
